c2147292157c0858e606e4ea5ee6b3fff29fa6ed
CmdBlock <54425466+CmdBlockZQG@users.noreply.github.com>
2025-01-21 16:29:25 +0800

    乘除法器BlackBox

---
+++ src/main/scala/core/exec/Div.scala
@@ -0,0 +1,55 @@
+package core.exec
+
+import chisel3._
+import chisel3.util._
+
+class Div extends Module {
+  val in = IO(new Bundle {
+    val ready = Output(Bool())
+    val valid = Input(Bool())
+    val sign = Input(Bool())
+    val a = Input(UInt(32.W))
+    val b = Input(UInt(32.W))
+  })
+  val out = IO(new Bundle {
+    val ready = Input(Bool())
+    val valid = Output(Bool())
+    val quot = Output(UInt(32.W))
+    val rem = Output(UInt(32.W))
+  })
+  val flush = IO(Input(Bool()))
+
+  class DivInternal extends BlackBox with HasBlackBoxResource {
+    val io = IO(new Bundle {
+      val clock = Input(Clock())
+      val reset = Input(Reset())
+      val flush = Input(Bool())
+
+      val in_ready = Output(Bool())
+      val in_valid = Input(Bool())
+      val in_sign = Input(Bool())
+      val in_a = Input(UInt(32.W))
+      val in_b = Input(UInt(32.W))
+
+      val out_ready = Input(Bool())
+      val out_valid = Output(Bool())
+      val out_quot = Output(UInt(32.W))
+      val out_rem = Output(UInt(32.W))
+    })
+    addResource("/MUL_test.sv")
+  }
+
+  val div = Module(new DivInternal)
+  div.io.clock := clock
+  div.io.reset := reset
+  div.io.flush := flush
+  in.ready := div.io.in_ready
+  div.io.in_valid := in.valid
+  div.io.in_sign := in.sign
+  div.io.in_a := in.a
+  div.io.in_b := in.b
+  div.io.out_ready := out.ready
+  out.valid := div.io.out_valid
+  out.quot := div.io.out_quot
+  out.rem := div.io.out_rem
+}
---
+++ src/main/scala/core/exec/Mul.scala
@@ -0,0 +1,52 @@
+package core.exec
+
+import chisel3._
+import chisel3.util._
+
+class Mul extends Module {
+  val in = IO(new Bundle {
+    val ready = Output(Bool())
+    val valid = Input(Bool())
+    val sign = Input(UInt(2.W))
+    val a = Input(UInt(32.W))
+    val b = Input(UInt(32.W))
+  })
+  val out = IO(new Bundle {
+    val ready = Input(Bool())
+    val valid = Output(Bool())
+    val prod = Output(UInt(64.W))
+  })
+  val flush = IO(Input(Bool()))
+
+  class MulInternal extends BlackBox with HasBlackBoxResource {
+    val io = IO(new Bundle {
+      val clock = Input(Clock())
+      val reset = Input(Reset())
+      val flush = Input(Bool())
+
+      val in_ready = Output(Bool())
+      val in_valid = Input(Bool())
+      val in_sign = Input(UInt(2.W))
+      val in_a = Input(UInt(32.W))
+      val in_b = Input(UInt(32.W))
+
+      val out_ready = Input(Bool())
+      val out_valid = Output(Bool())
+      val out_prod = Output(UInt(64.W))
+    })
+    addResource("/MUL_test.sv")
+  }
+
+  val mul = Module(new MulInternal)
+  mul.io.clock := clock
+  mul.io.reset := reset
+  mul.io.flush := flush
+  in.ready := mul.io.in_ready
+  mul.io.in_valid := in.valid
+  mul.io.in_sign := in.sign
+  mul.io.in_a := in.a
+  mul.io.in_b := in.b
+  mul.io.out_ready := out.ready
+  out.valid := mul.io.out_valid
+  out.prod := mul.io.out_prod
+}
